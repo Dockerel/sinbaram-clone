e2d6c88b80f3c50a25720968ac8b3bc93240ea36
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-11-28 18:10:42 +0900

    login, logout finished

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
     path("change-password", views.ChangePassword.as_view()),
+    path("login", views.Login.as_view()),
+    path("logout", views.Logout.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.views import APIView
 from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
@@ -48,3 +49,30 @@ class ChangePassword(APIView):
             return Response(status=HTTP_200_OK)
         else:
             raise ParseError
+
+
+class Login(APIView):
+    def post(self, request):
+        username = request.data["username"]
+        password = request.data["password"]
+        if not username or not password:
+            return ParseError
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user:
+            login(request, user)
+            return Response({"ok": "Welcome"})
+        else:
+            return Response({"error": "Wrong Password"})
+
+
+class Logout(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"ok": "Bye~"})
